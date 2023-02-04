fb2c3dd6334277220804f10407e39040f2254ace
Nick Wang <nickwang@eduroam-169-233-190-213.ucsc.edu>
2023-02-04 15:23:45 -0800

    YAY NEWJEANS

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 
-def infinite():
-	while true:
+def bunnies():
+	while True:
 		print("There is nothing wrong with the debut of the new hit korean girlgroup NewJeans and Haerin is the best member!")
 
-
+bunnies()
